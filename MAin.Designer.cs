f033584a857760dcb1c1d50a750a910b110c5d74
tbencardinop <63705622+tbencardinop@users.noreply.github.com>
2020-05-02 18:20:46 +1000

    Add project files.

---
+++ MAin.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DragNDrop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picDrop = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDrop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDrop
+            // 
+            this.picDrop.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.picDrop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picDrop.Location = new System.Drawing.Point(12, 12);
+            this.picDrop.Name = "picDrop";
+            this.picDrop.Size = new System.Drawing.Size(620, 264);
+            this.picDrop.TabIndex = 0;
+            this.picDrop.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(500, 282);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 34);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 328);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.picDrop);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drag & Drop";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.picDrop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDrop;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MAin.cs
@@ -0,0 +1,64 @@
+﻿//*****************************************************************************
+// Programmer: Tatiana Bencardino
+// Date: 30 April 2020
+// Software: Microsoft Visual Studio 2019 Community Edition
+// Platform: Microsoft Windows 10 Professional 64­bit
+// Purpose: Drag & drop of a file from Windows Explorer to a picture box
+//*****************************************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragNDrop
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;      //Enable drag and drop for this form
+            this.DragEnter += new DragEventHandler(frmMain_DragEnter);  //Event handler for drag enter
+            this.DragDrop += new DragEventHandler(frmMain_DragDrop);    //Event handler for drag drop
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileName = data as string[];
+                if (fileName.Length > 0)
+                    picDrop.Image = Image.FromFile(fileName[0]);
+            }
+        }
+    }
+}
